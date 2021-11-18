582ae22dcd7105e346b625e1a7e603f180376a07
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-11-18 22:55:15 +0530

    infra: setting up frontend

---
+++ packages/frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="p-6 items-center justify-center">
+      <h1 className="text-blue-400 font-extrabold">Hello World!</h1>
+      <p className="tracking-widest">Chess</p>
+    </div>
+  );
+}
+
+export default App;
